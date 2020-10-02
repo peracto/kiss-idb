bae28e5d51c9e3afa10d6e1ca388158cb180eb35
Gary <garyranson@hotmail.com>
2020-10-02 09:31:27 +0100

    Refector

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,33 @@
 type IDbRequestCallback = (store: IDBObjectStore) => IDBRequest
+type TransVal2 = <T>(type: IDBTransactionMode, cb: IDbRequestCallback) => Promise<T>
 
-interface TransVal {
-    ro<T>(cb: IDbRequestCallback): Promise<T>;
-
-    rw<T>(cb: IDbRequestCallback): Promise<T>;
-}
-
-const createIdbPromise = (dbName: string, storeName: string): Promise<TransVal> =>
-    new Promise((resolve, reject) => {
-        const r = indexedDB.open(dbName, 1)
-        r.onerror = () => reject(r.error)
-        r.onsuccess = () => resolve({
-            ro: create(r.result, "readonly", storeName),
-            rw: create(r.result, "readwrite", storeName)
+const createDatabase = (dbName: string, storeName: string) : Promise<IDBDatabase> =>
+        new Promise((resolve, reject) => {
+            const r = indexedDB.open(dbName, 1)
+            r.onerror = () => reject(r.error)
+            r.onsuccess = () => resolve(r.result)
+            r.onupgradeneeded = () => {
+                r.result.createObjectStore(storeName)
+            }
         })
-        r.onupgradeneeded = () => {
-            r.result.createObjectStore(storeName)
-        }
+
+const createTxnPromise = (dbp: Promise<IDBDatabase>, storeName: string) : TransVal2 =>
+    <T>(type: IDBTransactionMode, callback: IDbRequestCallback) => dbp.then(db => createPromise(db.transaction(storeName, type), storeName, callback))
+
+const createPromise = <T>(txn: IDBTransaction, storeName: string, callback: IDbRequestCallback) =>
+    new Promise<T>((resolve, reject) => {
+        txn.oncomplete = () => resolve(r.result as T)
+        txn.onabort = txn.onerror = () => reject(txn.error)
+        const r = callback(txn.objectStore(storeName))
     })
 
-const create = (db: IDBDatabase, type: IDBTransactionMode, storeName: string) =>
-    <T>(callback: IDbRequestCallback) => new Promise<T>((resolve, reject) => {
-        const t = db.transaction(storeName, type)
-        t.oncomplete = () => resolve(r.result as T)
-        t.onabort = t.onerror = () => reject(t.error)
-        const r = callback(t.objectStore(storeName))
-    })
-
-const _storeFactory = (db: Promise<TransVal>) => ({
-    get: <T>(key: IDBValidKey): Promise<T> => db.then(x => x.ro(s => s.get(key))),
-    set: <T>(key: IDBValidKey, value: T): Promise<T> => db.then(x => x.rw(s => s.put(value, key))),
-    del: (key: IDBValidKey) => db.then(x => x.rw(s => s.delete(key))),
-    clear: () => db.then(x => x.rw(store => store.clear())),
-    keys: (): Promise<IDBValidKey[]> => db.then(x => x.ro(store => store.getAllKeys()))
+const _storeFactory = (t: TransVal2) => ({
+    get: <T>(key: IDBValidKey): Promise<T> => t("readonly", s => s.get(key)),
+    set: <T>(key: IDBValidKey, value: T): Promise<T> => t("readwrite", s => s.put(value, key)),
+    del: (key: IDBValidKey) => t("readwrite", s => s.delete(key)),
+    clear: () => t("readwrite", s => s.clear()),
+    keys: (): Promise<IDBValidKey[]> => t("readonly", s => s.getAllKeys())
 })
 
 export const storeFactory = (dbName: string = 'kvp-store', storeName: string = 'kvp') =>
-    _storeFactory(createIdbPromise(dbName, storeName))
+    _storeFactory(createTxnPromise(createDatabase(dbName, storeName), storeName))
